feaaab8783c5b4af29d2072b305b44aaf099937c
WarmthDawn <warmth06@sina.com>
2021-11-15 15:36:26 +0800

    :tada: create project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+module.exports = {
+    root: true,
+    env: {
+        browser: true, // browser global variables
+        es2021: true, // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12.
+    },
+    parser: "vue-eslint-parser",
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'comma-dangle': ['error', 'always-multiline'],
+    },
+};
---
+++ src/plugins/naive.ts
@@ -0,0 +1,35 @@
+import {
+  create,
+  NLayout, NLayoutContent, NLayoutFooter, NLayoutHeader, NLayoutSider,
+  NSpace, NButton, NDivider, NIcon, NButtonGroup, NMessageProvider, NDropdown,
+  NBreadcrumb, NBreadcrumbItem, NInput, NInputGroup, NTree, NDataTable,
+  NUpload, NUploadTrigger, NUploadFileList, NCard, NModal,
+} from 'naive-ui'
+
+export default create({
+  components: [
+    NButton,
+    NButtonGroup,
+    NLayout,
+    NLayoutHeader,
+    NLayoutFooter,
+    NLayoutContent,
+    NLayoutSider,
+    NSpace,
+    NDivider,
+    NIcon,
+    NMessageProvider,
+    NDropdown,
+    NBreadcrumb,
+    NBreadcrumbItem,
+    NInput,
+    NInputGroup,
+    NTree,
+    NDataTable,
+    NUpload,
+    NUploadTrigger,
+    NUploadFileList,
+    NCard,
+    NModal,
+  ],
+})
